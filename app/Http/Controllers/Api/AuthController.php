ebbbba930033eb1e971124ed4f2f77479ea2dea1
Mohamed Siddig <mohamedsiddig915@gmail.com>
2025-06-22 18:07:08 +0200

    paitent authentication api, register & login workflow included

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -33,6 +33,13 @@ class AuthController extends Controller
         try{
             $patient = Patient::whereEmail($request->email)->first();
 
+            /**
+             * authenticate the patient by checking his credentials
+             * the if above check if there existed patient with the provided email
+             * the second prat is comparing the hashed password with plain password came for the request
+             * if both are true, then it's an authenticated patient
+             * the we return his basic info and sanctum token
+             *  */
             if(!$patient->exists() || !Hash::check($request->password, $patient->password)){
                 return response()->json([
                     'message' => 'Login failure, due to incorrect email or password',
@@ -43,8 +50,8 @@ class AuthController extends Controller
                     ]
                 ]);
             }
-            // revoke the old tokens
-            $patient->tokens()->delete();
+            // revoke the old tokens, if needed
+            // $patient->tokens()->delete();
 
             return response()->json([
                 'patient' => $patient->only([
